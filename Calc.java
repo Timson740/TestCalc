ad04e30822c54930ce47ba1ce585097c51a220e5
Timson740 <99961676+Timson740@users.noreply.github.com>
2022-02-18 13:32:57 +0300

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,29 @@
+import Java.util.Scanner;
+
+public class Calc {
+
+    String ldata = "";
+
+    public static void main(String[] args) {
+
+    Scanner data = new Scanner (System.in);
+    ldata = data.nextLine();
+
+    }
+
+    public static int tonumber (string a) {
+        switch (a.charAt(0)):
+        case 'X':
+            if a.charAt(1) != '':
+                System.out.println("Ошибка! Число не входит в диапазон!");
+            else:
+                return 10;
+            break;
+        case 'V':
+            switch (a.charAt(1)):
+        case 'I':
+
+
+    }
+
+}
